386e06e1e1802c559b658d0b1ae2d709533cf94e
benoitTallandier <benoit.tallandier@gmail.com>
2017-03-19 16:24:30 +0100

    design solution

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
 				<div class="col-sm-3 col-centered whenuplay">
 					<div class='item'>
 						<input type="text" id='word' style="width:150px" class="input-sm form-control" placeholder="Mot">
-						<div id='solution' style='backgroud-color:#DDDDDD;'></div>
+						<div id='solution'></div>
 					</div>
 				</div>
 			</div>
