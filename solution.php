bd80062e6dda6d02a0ae69a3cbd931fc0b1c156e
benoitTallandier <benoit.tallandier@gmail.com>
2017-03-19 16:15:51 +0100

    solution

--- solution.php
+++ solution.php
@@ -1,12 +1,11 @@
 <?php
     include("DBConnection.php");
-    if(isset($_GET['m'])){
-        $r = mysqli_query($db,"SELECT mot FROM vocabulaire WHERE mot LIKE '%".$_GET['m']."%' ORDER BY idMot DESC");
-        extract(mysqli_fetch_array($r));
-        $tmp = exec("python AnglaisBeuhBeuh.py $mot");
-        parse_str($tmp,$output);
-        echo "word : ".$mot."</br>";
-        echo "translation : ".$output['translation']."</br>";
-        echo "definition : ".$output['definition']."</br>";
-    }
+    extract(mysqli_fetch_array(mysqli_query($db,"SELECT model FROM user WHERE idUSer=".$_SESSION['user'])));
+    $r = mysqli_query($db,"SELECT mot FROM vocabulaire WHERE mot LIKE '%".$model."%' ORDER BY idMot DESC");
+    extract(mysqli_fetch_array($r));
+    $tmp = exec("python AnglaisBeuhBeuh.py $mot");
+    parse_str($tmp,$output);
+    echo "word : ".$mot."</br>";
+    echo "translation : ".$output['translation']."</br>";
+    echo "definition : ".$output['definition']."</br>";*/
 ?>
