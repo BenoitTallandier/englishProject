a85f3bfc6550b8c0139f6a1a23ef1adc7f1dd587
root <root@vps355596.ovh.net>
2017-03-19 16:18:08 +0100

    solution

--- solution.php
+++ solution.php
@@ -1,5 +1,6 @@
 <?php
     include("DBConnection.php");
+	session_start();
     extract(mysqli_fetch_array(mysqli_query($db,"SELECT model FROM user WHERE idUSer=".$_SESSION['user'])));
     $r = mysqli_query($db,"SELECT mot FROM vocabulaire WHERE mot LIKE '%".$model."%' ORDER BY idMot DESC");
     extract(mysqli_fetch_array($r));
@@ -7,5 +8,5 @@
     parse_str($tmp,$output);
     echo "word : ".$mot."</br>";
     echo "translation : ".$output['translation']."</br>";
-    echo "definition : ".$output['definition']."</br>";*/
+    echo "definition : ".$output['definition']."</br>";
 ?>
