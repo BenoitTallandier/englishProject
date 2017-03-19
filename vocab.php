32ea8c0957009f8d22e7f34eb1b962fb08b8a19f
benoitTallandier <benoit.tallandier@gmail.com>
2017-03-19 14:27:38 +0100

    thread

--- vocab.php
+++ vocab.php
@@ -4,11 +4,20 @@
 
 	class workerThread extends Thread{
 
-		public function __construct($b){
-			$this->i = $i;
+		public function __construct($word){
+			$this->word = $word;
 		}
 		public function run(){
-			
+			extract(mysqli_fetch_array($r));
+			$tmp = exec("python AnglaisBeuhBeuh.py $this->word");
+			parse_str($tmp,$output);
+			if(!empty($output['translation']) && !empty($output['definition'])){
+				echo "mot : ".$this->word."</br>";
+				flush();
+				mysqli_query($db,$query);
+				$query = "UPDATE vocabulaire SET definition='".$output['definition']."', translation='".$output['translation']."' WHERE mot=".$this->word;
+			}
+			exit();
 		}
 	}
 
@@ -21,16 +30,8 @@
 	$i=0;
 	while($l = mysqli_fetch_array($r)){
 		extract($l);
-		$word = $mot;
-		$tmp = exec("python AnglaisBeuhBeuh.py $word");
-		parse_str($tmp,$output);
-		if(!empty($output['translation']) && !empty($output['definition'])){
-			$per = floor($i/$total);
-			echo $per."%, mot : ".$word."</br>";
-			flush();
-			$query = "UPDATE vocabulaire SET definition='".$output['definition']."', translation='".$output['translation']."' WHERE idMot=".$idMot;
-			mysqli_query($db,$query);
-		}
+		$workers[$i]=new workerThread($word);
+		$workers[$i]->start();
 		$i++;
 	}
 
