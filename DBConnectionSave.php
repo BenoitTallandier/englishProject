3c176ba3b6c05cfbcdc70a9b002b31f04db1d0a9
benoitTallandier <benoit.tallandier@gmail.com>
2017-03-19 21:01:01 +0100

    del db

--- DBConnectionSave.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host_name  = "137.74.194.232";
-$database   = "anglais";
-$user_name  = "remoteAcces";
-$password   = "paozie";
-$db = mysqli_connect($host_name, $user_name, $password, $database);
-if(mysqli_connect_errno()) {die('The connection to the database could not be established.');}
-$db->set_charset("utf8");
-?>
